e87dc5ed5a522264b8283330d76eccc82a3be8e0
Tim Miller <tim@rocket5studios.com>
2025-03-31 15:58:55 -0400

    Update vite.config.js to include privacy policy and thank you pages in build

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        privacy: resolve(__dirname, 'src/privacy-policy.html'),
+        thankYou: resolve(__dirname, 'src/thank-you.html'),
       },
     },
     assetsInlineLimit: 0,
